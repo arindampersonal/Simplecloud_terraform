e22dc2a32a586ad1b3c47d8e56fc5d4fca4ae870
Arindam <arindaminaction@gmail.com>
2026-05-21 01:38:33 +0530

    First Commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resource-group"
+  location = "East US"
+}
