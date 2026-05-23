c997dc9784f3f1b41235f44b3164f8c919b19c16
Arindam <arindaminaction@gmail.com>
2026-05-23 08:09:00 +0530

    Import existing resource groups into Terraform state

---
+++ imports.tf
@@ -0,0 +1,9 @@
+import {
+  to = azurerm_resource_group.example
+  id = "/subscriptions/1059dab0-bad6-4abd-a766-d44eb504748d/resourceGroups/my-resource-group"
+}
+
+import {
+  to = azurerm_resource_group.vnet-resource
+  id = "/subscriptions/1059dab0-bad6-4abd-a766-d44eb504748d/resourceGroups/my-vnet-resource-group"
+}
